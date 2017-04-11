df65392cf627f591cd62c8f8c6352543177af005
realBH <bhewitt101@gmail.com>
2017-04-11 20:21:59 +0000

    Added create functionality as well as user messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,20 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect # renders to screen
     @article = Article.new(article_params) #Need to whitelist values of Article
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article Successfully Created!"
+      redirect_to article_path(@article)
+    else
+      #flash[:notice] = "You are dumb, try again"
+      render 'new'
+    end
+    
+    #@article.save
+    #redirect_to article_path(@article)
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
