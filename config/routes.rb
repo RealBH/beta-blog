6ecc45ea1ca4a21f1b19a95f3f6830b3fd77d3ba
realBH <bhewitt101@gmail.com>
2017-04-11 00:40:08 +0000

    made some buttons woohoo

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 root 'pages#home'
 get 'pages/about', to: 'pages#about'
 
+resources :articles # Gives new article path, post to create
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
